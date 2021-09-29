6f26691f79a8625aa50854759a8271b4ffdbf1f5
Martin Kadlec <harou2@gmail.com>
2021-09-29 14:45:11 +0200

    Add next-pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
 module.exports = {
 	images: {
 		domains: ['emoji.gg'],
+	},
+	pwa: {
+		dest: 'public',
+		runtimeCaching,
 	},
 };
