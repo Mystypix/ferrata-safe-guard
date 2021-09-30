16907060d8de6dc1307f5c36be948d84c7ff5f46
Martin Kadlec <martin.kadlec@socialbakers.com>
2021-09-30 11:01:45 +0200

    Remember sesions between reloads

--- src/services/session/session.js
+++ src/services/session/session.js
@@ -1,6 +1,13 @@
+let currentSession = null
+if (global.localStorage) {
+	currentSession = localStorage.getItem('session')
+	currentSession = currentSession ? JSON.parse(currentSession) : null
+}
+
 const session = {
-	_user: null,
+	_user: currentSession,
 	setUser: function (user) {
+		localStorage.setItem('session', user === null ? null : JSON.stringify(user))
 		this._user = user;
 	},
 	getUser: function () {
