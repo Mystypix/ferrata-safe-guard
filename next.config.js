83533fbdfa695324cbd5d7c49625db79e55ff3b2
Jakub Smagin <jakub.smagin@socialbakers.com>
2021-09-30 12:24:02 +0200

    asjdskj

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
+// const withPWA = require('next-pwa');
+// const runtimeCaching = require('next-pwa/cache');
 
 module.exports = {
-	pwa: {
-		dest: 'public',
-		runtimeCaching,
-	},
+	// pwa: {
+	// 	dest: 'public',
+	// 	runtimeCaching,
+	// },
 	eslint: {
 		ignoreDuringBuilds: true,
 	}
