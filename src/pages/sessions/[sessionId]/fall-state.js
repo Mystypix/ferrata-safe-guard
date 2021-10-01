fc81b7ac50303c6b6aa3440d945e31eb0843087a
Martin Kadlec <harou2@gmail.com>
2021-10-01 09:22:13 +0200

    fix build

--- src/pages/sessions/[sessionId]/fall-state.js
+++ src/pages/sessions/[sessionId]/fall-state.js
@@ -1,23 +1,23 @@
-import classnames from 'classnames'
+import classnames from 'classnames';
 import {useEffect, useState} from 'react';
-import Image from 'next/image'
-import Button, {BUTTON_TYPE} from 'components/button'
+import Image from 'next/image';
+import Button, {BUTTON_TYPE} from 'components/button';
 import {Duration} from 'luxon';
 
 const formatDuration = duration => {
 	return Duration.fromMillis(duration * 1000).toFormat('mm:ss');
 };
 
-const COUNTDOWN_TIME = 20
+const COUNTDOWN_TIME = 20;
 
 function FallState(props) {
-	const [time, setTime] = useState(COUNTDOWN_TIME)
+	const [time, setTime] = useState(COUNTDOWN_TIME);
 
 	useEffect(() => {
 		const interval = setInterval(() => {
 			setTime(time => time - 1);
 			if (time === 0) {
-				props.openActiveState()
+				props.openActiveState();
 			}
 		}, 1000);
 		return () => {
@@ -26,17 +26,22 @@ function FallState(props) {
 	}, [time, setTime]);
 
 	function handleClick() {
-		props.cancelFallState()
+		props.cancelFallState();
 	}
 
 	return (
 		<div>
-			<Image src='/icons/accident.svg' width='312' height='244' />
+			<Image src="/icons/accident.svg" width="312" height="244" />
 			<div>Fall detected</div>
-			<div>Sending for help in {formatDuration(timer)}</div>
-			<Button className={classnames(BUTTON_TYPE.action)} onClick={() => handleClick}>Everything is fine</Button>
+			<div>Sending for help in {formatDuration(time)}</div>
+			<Button
+				className={classnames(BUTTON_TYPE.action)}
+				onClick={() => handleClick}
+			>
+				Everything is fine
+			</Button>
 		</div>
-	)
+	);
 }
 
-export default FallState
+export default FallState;
