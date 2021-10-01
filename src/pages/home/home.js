646b2c95ff9e4cc3f17a18c36c6582b7ff01b299
Martin Kadlec <harou2@gmail.com>
2021-10-01 10:38:27 +0200

    app install, attempt 1

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,8 +2,37 @@ import Link from 'next/link';
 import Image from 'next/image';
 import css from './home.module.scss';
 import Button from 'components/button';
+import {useRef, useEffect} from 'react';
+
+const IS_STANDALONE =
+	global.matchMedia && global.matchMedia('(display-mode: standalone)').matches;
 
 function HomePage() {
+	const defferedPromptRef = useRef();
+	const handleBeforeInstall = event => {
+		event.preventDefault();
+		defferedPromptRef.current = event;
+	};
+	useEffect(() => {
+		let defferedPrompt;
+		window.addEventListener('beforeinstallprompt', handleBeforeInstall);
+		() =>
+			window.removeEventListener('beforeinstallprompt', handleBeforeInstall);
+	}, []);
+
+	const handleInstall = ev => {
+		const defferedPrompt = defferedPromptRef.current;
+		if (!defferedPrompt) return;
+		defferedPrompt.prompt();
+
+		defferedPrompt.userChoice.then(choice => {
+			if (choice.outcome === 'accepted') {
+				console.log('user accepted the prompt');
+			}
+			defferedPrompt = null;
+		});
+	};
+
 	return (
 		<div className={css.homePage}>
 			<Image src="/icons/fall.svg" width="312" height="244" />
@@ -14,6 +43,12 @@ function HomePage() {
 				theme and indicated by a new line, indentation, or numbering.
 			</p>
 			<div className={css.actions}>
+				{!IS_STANDALONE && (
+					<Button size="large" type="primary" onClick={handleInstall}>
+						Install App
+					</Button>
+				)}
+
 				<Link href="/login">
 					<Button size="large" type="action">
 						Let's Start{' '}
